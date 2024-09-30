ae3cec1e5f134063084d1c21b4d03c9a50360b4f
vashukashyap <kashyapvashu2003@gmail.com>
2024-09-30 23:56:40 +0530

    added a example navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import BackgroundComponent from './components/background';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
+      <Navbar/>
       <BackgroundComponent>
       <h1 className='bg-black'>Hello everyone
 
